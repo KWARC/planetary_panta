7f836e0238a13b4aa58476bf8ec2e0cb9b3839f9
Mihai Fieraru <fierarufmihai@gmail.com>
2013-11-25 14:08:04 +0100

    Announcements + Documentation + Red link label + Red label in content

--- sites/all/modules/stack_overflow/views-view-field--stack-overflow-list--field-number-of-answers.tpl.php
+++ sites/all/modules/stack_overflow/views-view-field--stack-overflow-list--field-number-of-answers.tpl.php
@@ -37,27 +37,51 @@
 
     $ans_not_viewed = 0;
     $no_ans = 0;
+    $first = -1;
 
     foreach($views_ans->result as $res2){
       $no_ans++;
       $id_ans = $res2->nid;
       $ans_visits = node_view_count_count_node_view(node_load($id_ans), $user) ;
-      if($ans_visits == 0) $ans_not_viewed++;
+      if($ans_visits == 0) {
+        $x = node_load($id_ans);
+
+        if($first == -1)
+          // first is not set yet
+          $first = $id_ans;
+        
+        else {
+          // first is set, we check if we find a previous one
+
+          $first_node = node_load($first);
+          if( date_create('@'.$x->created) < date_create('@'.$first_node->created) )
+            $first = $id_ans;
+        }
+
+
+
+
+        $ans_not_viewed++;
+      }
     }
-    //dpm($no_ans);
+    
 
 
  
     if(count($row->field_field_number_of_answers) > 0){
     	print $no_ans;
- 		if ($ans_not_viewed > 0) {
- 				$datetime_develop = date_create('2013-10-06');
- 				//  Apply this only since datetime_develop
- 				// We don't want all the previous question to look unread
+   		if ($ans_not_viewed > 0) {
+   				$datetime_develop = date_create('2013-10-06');
+   				//  Apply this only since datetime_develop
+   				// We don't want all the previous question to look unread
 
- 				if ($datetime_develop  < date_create('@'.$row->node_created))
- 		   			print "<div style='color:red;'><strong>".$ans_not_viewed." new</strong></div>";
- 		   	}   	
+   				if ($datetime_develop  < date_create('@'.$row->node_created)){
+              print "<style>   a.stacklink:link {color:#ff0000;}   a.stacklink:visited {color:#ff0000;}  a.stacklink:hover {color:#ff0000;}     </style>";
+              print "<div><strong>".l(t($ans_not_viewed." new"), 'node/'.$first, array('attributes' => array('class' => 'stacklink')))."</strong></div>";
+          }
+
+
+        }   	
     }
 
 
