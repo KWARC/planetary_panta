b45edab097e8874a853a3c844c53d804d6cc7162
Mihai Fieraru <fierarufmihai@gmail.com>
2014-03-09 00:19:21 +0100

    replacement of username with full name + partial resources upload functionality

--- sites/all/modules/course_creation/course_description.tpl.php
+++ sites/all/modules/course_creation/course_description.tpl.php
@@ -12,32 +12,14 @@
 	<?php echo drupal_render($content["my_additional_field"]); ?>
 	</div>
 	
-	<div style="border-top:1px solid #313030;">
-	<?php echo drupal_render($content["field_lecture_hours"]); ?>
-	</div>
 
 	<div style="border-top:1px solid #313030;">
 	<?php echo drupal_render($content["field_summary"]); ?>
 	</div>
 
-	<div style="border-top:1px solid #313030;">
-	<?php echo drupal_render($content["field_course_materials"]); ?>
-	</div>
 
 	<div style="border-top:1px solid #313030;">
-	<?php echo drupal_render($content["field_schedule"]); ?>
-	</div>
-
-	<div style="border-top:1px solid #313030;">
-	<?php echo drupal_render($content["field_grading"]); ?>
-	</div>
-
-	<div style="border-top:1px solid #313030;">
-	<?php echo drupal_render($content["field_assignments"]); ?>
-	</div>
-
-	<div style="border-top:1px solid #313030;">
-	<?php echo drupal_render($content["field_exams"]); ?>
+	<?php echo drupal_render($content["field_resources"]); ?>
 	</div>
 
 
